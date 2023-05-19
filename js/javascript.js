b610f4aeaa49e7cb4a6957e3ab2dfef6daf25efe
Exudias <editoo31@gmail.com>
2023-05-19 21:08:53 +0300

    Create operate function and appropriate variables

--- js/javascript.js
+++ js/javascript.js
@@ -17,3 +17,22 @@ function divide(a, b)
 {
     return a / b;
 }
+
+function operate(num1, operator, num2)
+{
+    switch(operator)
+    {
+        case "add":
+            return add(num1, num2);
+        case "subtract":
+            return subtract(num1, num2);
+        case "multiply":
+            return multiply(num1, num2);
+        case "divide":
+            return divide(num1, num2);
+        default:
+            throw Error("Incorrect operator!");
+    }
+}
+
+let num1, operator, num2;
